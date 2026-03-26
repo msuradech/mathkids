a6e15543d997adfb3b15a4a22350abaf80a6259e
suradech.wsl <m.suradech@gmail.com>
2026-03-26 14:46:15 +0700

    feat(oci): config connection to use .env

--- app/db/oci.py
+++ app/db/oci.py
@@ -1,17 +1,19 @@
 import oracledb
 import os
 
-# 🔥 ปรับค่าตรงนี้ให้ตรงกับ DB ของมึง
-DB_USER = "your_user"
-DB_PASSWORD = "your_password"
-DB_DSN = "localhost:1521/XEPDB1"  
-# หรือแบบ OCI:
-# DB_DSN = "hostname:1522/your_service_name"
+DB_USER = os.getenv("OCI_USER")
+DB_PASSWORD = os.getenv("OCI_PASSWORD")
+WALLET_PASSWORD = os.getenv("WALLET_PASSWORD")
+DB_DSN = "OCI"
+WALLET_PATH = "./wallet"
 
 pool = oracledb.create_pool(
     user=DB_USER,
     password=DB_PASSWORD,
     dsn=DB_DSN,
+    config_dir=WALLET_PATH,
+    wallet_location=WALLET_PATH,
+    wallet_password=WALLET_PASSWORD,
     min=2,
     max=5,
     increment=1
