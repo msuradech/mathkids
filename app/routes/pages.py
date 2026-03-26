ef668610767cce74151cf2cbb9bb1f3fb56d0dc8
Suradech_NB <m.suradech@gmail.com>
2026-03-26 10:21:45 +0700

    refactor: change param name level to mode

--- app/routes/pages.py
+++ app/routes/pages.py
@@ -1,20 +1,10 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from app.services.quiz_service import generate_questions
+from app.services.quiz_service import generate_questions, get_mode_config
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
-
-
-def get_level_config(level: str):
-    config = {
-        "easy": 10,
-        "normal": 20,
-        "hard": 50
-    }
-    return config.get(level, 10)
-
 
 @router.get("/", response_class=HTMLResponse)
 def home(request: Request):
@@ -24,9 +14,9 @@ def home(request: Request):
     context={}
 )
 
-@router.get("/quiz/{level}", response_class=HTMLResponse)
-def quiz(request: Request, level: str):
-    total = get_level_config(level)
+@router.get("/quiz/{mode}", response_class=HTMLResponse)
+def quiz(request: Request, mode: str):
+    total = get_mode_config(mode)
     questions = generate_questions(total)
 
     return templates.TemplateResponse(
@@ -34,6 +24,6 @@ def quiz(request: Request, level: str):
     name="index.html",
     context={
         "questions": questions,
-        "level": level
+        "mode": mode
     }
 )
--- app/services/quiz_service.py
+++ app/services/quiz_service.py
@@ -1,5 +1,12 @@
 import random
 
+def get_mode_config(mode: str):
+    config = {
+        "easy": 10,
+        "normal": 20,
+        "hard": 50
+    }
+    return config.get(mode, 10)
 
 def generate_single_question():
     a = random.randint(0, 9)
@@ -16,6 +23,7 @@ def generate_single_question():
         "answer": answer
     }
 
-
 def generate_questions(n: int):
     return [generate_single_question() for _ in range(n)]
+
+
