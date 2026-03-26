0fbaeb9de786a3481ebad5acbcfc0831a64686bd
suradech.wsl <m.suradech@gmail.com>
2026-03-26 13:36:59 +0700

    feat: display timing and send result/score to dummy API

--- app/routes/pages.py
+++ app/routes/pages.py
@@ -1,10 +1,18 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 from app.services.quiz_service import generate_questions, get_mode_config
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
+
+class QuizResult(BaseModel):
+    level: str
+    totalQuestions: int
+    score: int
+    totalTime: int
+    avgTime: float
 
 @router.get("/", response_class=HTMLResponse)
 def home(request: Request):
@@ -13,6 +21,16 @@ def home(request: Request):
     name="index.html",
     context={}
 )
+
+@router.post("/quiz/record")
+def receive_quiz_result(data: QuizResult):
+    print("=== Quiz Result ===")
+    print(data)
+
+    return {
+        "status": "ok",
+        "message": "received"
+    }
 
 @router.get("/quiz01/{mode}", response_class=HTMLResponse)
 def quiz(request: Request, mode: str):
