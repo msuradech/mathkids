f62b01791636f0bb13f8f57652a2063203637a9b
suradech.wsl <m.suradech@gmail.com>
2026-04-09 16:49:33 +0700

    feat(score): show score at sidebar plain design(frontend)

--- app/static/js/utils/utils.js
+++ app/static/js/utils/utils.js
@@ -8,7 +8,63 @@ export async function loadUser() {
     const logoutBtn = document.getElementById("logout-btn");
 
     if (data.user) {
-        userDiv.innerText = "Logged in as: " + data.user.username;
+        const userId = data.user.user_id;
+        const monthlyScoreRes = await fetch(`/score/monthly/${userId}`);
+        const monthlyScoreData = await monthlyScoreRes.json();
+
+        let monthlyScoreText = "";
+
+        if (monthlyScoreData.data.length > 0) {
+            const s = monthlyScoreData.data[0];
+
+            const score = s.score;
+            const minutes = (s.sec / 60).toFixed(2);
+            const avg = Number(s.avg_score).toFixed(2);
+
+            monthlyScoreText = `
+                <br>
+                <br>
+                <u>Monthly Score</u>
+                <br>
+                Score: ${score}
+                <br>
+                Time: ${minutes} min
+                <br>
+                Avg: ${avg}
+            `;
+        }
+
+        const dailyScoreRes = await fetch(`/score/daily/${userId}`);
+        const dailyScoreData = await dailyScoreRes.json();
+
+        let dailyScoreText = "";
+
+        if (dailyScoreData.data.length > 0) {
+            const s = dailyScoreData.data[0];
+
+            const score = s.score;
+            const minutes = (s.sec / 60).toFixed(2);
+            const avg = Number(s.avg_score).toFixed(2);
+
+            dailyScoreText = `
+                <br>
+                <br>
+                <u>Daily Score</u>
+                <br>
+                Score: ${score}
+                <br>
+                Time: ${minutes} min
+                <br>
+                Avg: ${avg}
+            `;
+        }
+
+        userDiv.innerHTML = `
+            Logged in as: ${data.user.username}
+            ${monthlyScoreText}
+            ${dailyScoreText}
+        `;
+
         logoutBtn.style.display = "block";
     } else {
         userDiv.innerHTML = `
